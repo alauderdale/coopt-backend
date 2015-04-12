32c12a519ffcb3dcd7528ca63c080d17b737de22
Alex Lauderdale <alauderdale@mac.com>
2015-04-12 13:46:06 -0600

    build

---
+++ tmp/javascripts/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+	 //fancybox
+	$('.fancybox').fancybox();
+	
+	$('.fb-video').fancybox({
+        padding: 0,
+        width: "100%",
+        height: 870,
+        autoSize: false,
+        helpers: {
+            media: {}
+        }
+   });
+
+	$('.tooltip-up').tooltip({html:true});
+
+
+    $('a.units').click(function() {
+        $('a.units').removeClass('selected');
+        $('a.units .md').removeClass('md-radio-button-on');
+        $('a.units').addClass('unselected');
+        $(this).addClass('selected');
+        $(this).children('.md').addClass('md-radio-button-on');
+
+
+        return false; 
+    });
+
+});
