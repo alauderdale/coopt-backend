40bef962f64181ec5cc568f834f4ab328ddea653
Alex Lauderdale <alauderdale@mac.com>
2015-04-12 13:45:52 -0600

    picker

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -15,4 +15,16 @@ $(document).ready(function(){
 
 	$('.tooltip-up').tooltip({html:true});
 
+
+    $('a.units').click(function() {
+        $('a.units').removeClass('selected');
+        $('a.units .md').removeClass('md-radio-button-on');
+        $('a.units').addClass('unselected');
+        $(this).addClass('selected');
+        $(this).children('.md').addClass('md-radio-button-on');
+
+
+        return false; 
+    });
+
 });
